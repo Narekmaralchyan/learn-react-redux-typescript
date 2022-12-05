5816a7ea8344337ef4815c53090582ac9f10e6f5
Narekmaralchyan <narekmaralchyan@gmail.com>
2022-12-05 17:20:38 +0400

    new config

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import React, {useEffect, useState} from 'react';
 function App() {
 
   const [data,setData] = useState();
+  const [loading,setLoading] = useState(false);
   useEffect(() => {
-    fetch("https://mockend.com/org/repo/users")
+
+    fetch("https://mockend.com/Narekmaralchyan/learn-react-redux-typescript/users")
         .then(res => {
           console.log(res)
             return res.json()
@@ -14,11 +16,29 @@ function App() {
             console.log(data)
             setData(data)
         })
-  },[]);
+  },[loading]);
+
+  function addUser(){
+      fetch("https://mockend.com/Narekmaralchyan/learn-react-redux-typescript/users", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: "Narek",
+        age:25,
+          id:"narekjan"
+      })
+  },[])
+          .then(res=>res.json())
+            .then(data=>{
+                setLoading(prev=>!prev)
+            })
+  }
 
   return (
     <div className="App">
-
+        <button onClick={addUser}>ADD</button>
     </div>
   );
 }
