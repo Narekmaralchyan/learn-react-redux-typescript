5390596936de6a3725af3418b46b8ee5b957805a
Narekmaralchyan <narekmaralchyan@gmail.com>
2022-12-05 16:55:58 +0400

    edit json file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import './App.css';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 function App() {
+
+  const [data,setData] = useState();
+  useEffect(() => {
+    fetch("https://mockend.com/org/repo/users")
+        .then(res => {
+          console.log(res)
+            return res.json()
+        })
+        .then(data => {
+            console.log(data)
+            setData(data)
+        })
+  },[]);
+
   return (
     <div className="App">
 
